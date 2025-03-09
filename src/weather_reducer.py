2a26a9093a3b0658da0d0b266b36d341d21da99a
Ramakrishna0977 <144200835+Ramakrishna0977@users.noreply.github.com>
2025-03-09 14:25:06 +0530

    Update weather_reducer.py

--- src/weather_reducer.py
+++ src/weather_reducer.py
@@ -1,1 +1,34 @@
+# src/weather_reducer.py
 
+import sys
+
+current_location = None
+temperatures = []
+humidities = []
+
+for line in sys.stdin:
+    line = line.strip()
+    location, temperature, humidity = line.split("\t")
+    try:
+        temperature = float(temperature)
+        humidity = float(humidity)
+    except ValueError:
+        continue
+
+    if current_location == location:
+        temperatures.append(temperature)
+        humidities.append(humidity)
+    else:
+        if current_location:
+            avg_temp = sum(temperatures) / len(temperatures)
+            avg_humidity = sum(humidities) / len(humidities)
+            print(f"{current_location},{avg_temp},{avg_humidity}")
+
+        current_location = location
+        temperatures = [temperature]
+        humidities = [humidity]
+
+if current_location:
+    avg_temp = sum(temperatures) / len(temperatures)
+    avg_humidity = sum(humidities) / len(humidities)
+    print(f"{current_location},{avg_temp},{avg_humidity}")
