874525af1895fed218dae5c7d713c6fb42902cff
Ramakrishna0977 <144200835+Ramakrishna0977@users.noreply.github.com>
2025-03-09 14:25:34 +0530

    Update weather_mapper.py

--- src/weather_mapper.py
+++ src/weather_mapper.py
@@ -1,1 +1,9 @@
+# src/weather_mapper.py
+import sys
 
+for line in sys.stdin:
+    line = line.strip()
+    if not line or line.startswith("location"):
+        continue  # Skip header or empty lines
+    location, timestamp, temperature, humidity = line.split(",")
+    print(f"{location}\t{temperature}\t{humidity}")
